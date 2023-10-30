4a0e281785346400bb22e726da22183e5ff0fde9
nonye-solos <awesontrug@gmail.com>
2023-10-30 19:15:42 +0000

    (My third commit)

---
+++ week-3/practice-10/target/debug/practice-10.d
@@ -0,0 +1,1 @@
+C:\Users\Chukwunonye\ Ejekam\Documents\c.ejekamCOS101\week-3\practice-10\target\debug\practice-10.exe: C:\Users\Chukwunonye\ Ejekam\Documents\c.ejekamCOS101\week-3\practice-10\src\main.rs
---
+++ week-3/practice-6/target/debug/practice-6.d
@@ -0,0 +1,1 @@
+C:\Users\Chukwunonye\ Ejekam\Documents\c.ejekamCOS101\week-3\practice-6\target\debug\practice-6.exe: C:\Users\Chukwunonye\ Ejekam\Documents\c.ejekamCOS101\week-3\practice-6\src\main.rs
